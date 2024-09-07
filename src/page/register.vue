95c0413099d1eb1c364e43a4f6a7424022d7686f
Luckiiest <59350999+Luckiiest@users.noreply.github.com>
2024-09-07 12:56:21 +0800

    update

--- src/page/register.vue
+++ src/page/register.vue
@@ -2,35 +2,38 @@
   <div class="page-register">
     <div class="header">
       <header>
-        <a href="#"
-           class="site-logo">美团</a>
+        <a href="#" class="site-logo">美团</a>
         <div class="login">
           <span>已有美团账号?</span>
-          <router-link :to="{name: 'login'}">登陆</router-link>
+          <router-link :to="{ name: 'login' }">登陆</router-link>
         </div>
       </header>
     </div>
     <div class="content">
-      <el-form :model="ruleForm"
-               status-icon
-               :rules="rules"
-               ref="ruleForm"
-               label-width="100px"
-               class="demo-registerForm">
-        <el-form-item label="用户名"
-                      prop="userName">
-          <el-input type="text"
-                    v-model="ruleForm.userName"
-                    autocomplete="off"></el-input>
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-registerForm"
+      >
+        <el-form-item label="用户名" prop="userName">
+          <el-input
+            type="text"
+            v-model="ruleForm.userName"
+            autocomplete="off"
+          ></el-input>
         </el-form-item>
-        <el-form-item label="密码"
-                      prop="pass">
-          <el-input type="password"
-                    v-model="ruleForm.pass"
-                    autocomplete="off"
-                    @input="input"></el-input>
+        <el-form-item label="密码" prop="pass">
+          <el-input
+            type="password"
+            v-model="ruleForm.pass"
+            autocomplete="off"
+            @input="input"
+          ></el-input>
           <div class="pw-strength">
-            <div :class="['bar',strengthClass]"></div>
+            <div :class="['bar', strengthClass]"></div>
             <div class="letter">
               <span>弱</span>
               <span>中</span>
@@ -38,124 +41,124 @@
             </div>
           </div>
         </el-form-item>
-        <el-form-item label="确认密码"
-                      prop="checkPass">
-          <el-input type="password"
-                    v-model="ruleForm.checkPass"
-                    autocomplete="off"></el-input>
+        <el-form-item label="确认密码" prop="checkPass">
+          <el-input
+            type="password"
+            v-model="ruleForm.checkPass"
+            autocomplete="off"
+          ></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary"
-                     @click="submitForm('ruleForm')">提交</el-button>
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >提交</el-button
+          >
         </el-form-item>
       </el-form>
     </div>
-    <div class="footer">
-
-    </div>
+    <div class="footer"></div>
   </div>
 </template>
 
 <script>
-import api from "@/api/index.js"
+import api from "@/api/index.js";
 export default {
-  data () {
+  data() {
     var userName = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入密码'));
+      if (value === "") {
+        callback(new Error("请输入密码"));
       } else if (value.length < 4 && value.length > 16) {
-        callback(new Error('用户名长度位4-16位数字字母下划线组成!'));
-      }
-      else {
-        if (this.ruleForm.userName !== '') {
-          this.$refs.ruleForm.validateField('checkPass');
+        callback(new Error("用户名长度位4-16位数字字母下划线组成!"));
+      } else {
+        if (this.ruleForm.userName !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
         }
         callback();
       }
     };
     var validatePass = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入密码'));
+      if (value === "") {
+        callback(new Error("请输入密码"));
       } else if (value.length < 4 && value.length > 16) {
-        callback(new Error('密码长度位6-16位数字字母下划线组成!'));
+        callback(new Error("密码长度位6-16位数字字母下划线组成!"));
       } else {
-        if (this.ruleForm.checkPass !== '') {
-          this.$refs.ruleForm.validateField('checkPass');
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
         }
         callback();
       }
     };
     var validatePass2 = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请再次输入密码'));
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
       } else if (value !== this.ruleForm.pass) {
-        callback(new Error('两次输入密码不一致!'));
+        callback(new Error("两次输入密码不一致!"));
       } else {
         callback();
       }
-    }
+    };
 
     return {
-      strengthClass: '',
+      strengthClass: "",
 
       ruleForm: {
-        pass: '',
-        checkPass: '',
-        userName: ''
+        pass: "",
+        checkPass: "",
+        userName: "",
       },
       rules: {
-        pass: [
-          { validator: validatePass, trigger: 'blur' }
-        ],
-        checkPass: [
-          { validator: validatePass2, trigger: 'blur' }
-        ],
-        userName: [
-          { validator: userName, trigger: 'blur' }
-        ]
-      }
-    }
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+        userName: [{ validator: userName, trigger: "blur" }],
+      },
+    };
   },
   methods: {
-    submitForm (formName) {
+    submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          api.register({
-            params: this.ruleForm
-          }).then(res => {
-            if (res.data.status == 'success') {
-              this.$router.push({ name: "login" });
-
-            } else {
-              alert(res.data.msg);
-            }
-          })
+          api
+            .register({
+              params: this.ruleForm,
+            })
+            .then((res) => {
+              if (res.data.status == "success") {
+                this.$router.push({ name: "login" });
+              } else {
+                alert(res.data.msg);
+              }
+            });
         } else {
-          console.log('注册失败');
+          console.log("注册失败");
           return false;
         }
       });
     },
-    input () {
+    input() {
       var regStr = /(\w)+/g;
       var regNum = /(\d)+/g;
       var reg = /(_)+/g;
-      var strongth = this.ruleForm.pass.match(reg) && this.ruleForm.pass.match(regStr) && this.ruleForm.pass.match(regNum);
+      var strongth =
+        this.ruleForm.pass.match(reg) &&
+        this.ruleForm.pass.match(regStr) &&
+        this.ruleForm.pass.match(regNum);
 
-      if (this.ruleForm.pass.length > 20 || (this.ruleForm.pass.length > 6 && strongth)) {
+      if (
+        this.ruleForm.pass.length > 20 ||
+        (this.ruleForm.pass.length > 6 && strongth)
+      ) {
         this.strengthClass = "strong";
       } else if (this.ruleForm.pass.length < 8) {
-        this.strengthClass = 'week'
+        this.strengthClass = "week";
       } else if (this.ruleForm.pass == "") {
-        this.strengthClass = '';
+        this.strengthClass = "";
       } else {
-        this.strengthClass = 'normal';
+        this.strengthClass = "normal";
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-@import '@/assets/css/register/index.scss';
+@import "@/assets/css/register/index.scss";
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,63 +1,63 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import defaultPage from '@/layout/default.vue'
-const blankPage = () => import('@/layout/blank.vue')
+import Vue from "vue";
+import VueRouter from "vue-router";
+import defaultPage from "@/layout/default.vue";
+const blankPage = () => import("@/layout/blank.vue");
 
-const Index = () => import('@/page/indexView.vue') // 首页
-const ChangeCity = () => import('@/page/changeCity.vue') // 切换城市页面
-const GoodsList = () => import('@/page/goodsList.vue') // 商品列表
-const Login = () => import('@/page/login.vue') //登陆
-const Register = () => import('@/page/register.vue') //注册
+const Index = () => import("@/page/indexView.vue"); // 首页
+const ChangeCity = () => import("@/page/changeCity.vue"); // 切换城市页面
+const GoodsList = () => import("@/page/goodsList.vue"); // 商品列表
+const Login = () => import("@/page/login.vue"); //登陆
+const Register = () => import("@/page/register.vue"); //注册
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'defaultPage',
+    path: "/",
+    name: "defaultPage",
     component: defaultPage,
-    redirect: '/index',
+    redirect: "/index",
     children: [
       {
-        path: '/index',
-        name: 'index',
+        path: "/index",
+        name: "index",
         component: Index,
       },
       {
-        path: '/changeCity',
-        name: 'changeCity',
+        path: "/changeCity",
+        name: "changeCity",
         component: ChangeCity,
       },
       {
-        path: 's/:name',
-        name: 'goods',
+        path: "s/:name",
+        name: "goods",
         component: GoodsList,
       },
     ],
   },
   {
-    path: '/blank',
-    name: 'blankPage',
+    path: "/blank",
+    name: "blankPage",
     component: blankPage,
     children: [
       {
-        path: 'login',
-        name: 'login',
+        path: "login",
+        name: "login",
         component: Login,
       },
       {
-        path: 'register',
-        name: 'register',
+        path: "register",
+        name: "register",
         component: Register,
       },
     ],
   },
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
-})
+});
 
-export default router
+export default router;
